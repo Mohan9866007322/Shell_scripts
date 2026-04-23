29481a01815888c1f34085696d2e70ab268744d3
Mohan9866007322 <mohan6117vja@gmail.com>
2026-04-23 09:29:56 +0530

    version 0.3

--- prac1.sh
+++ prac1.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 echo "Welcome to Nutanix"
 find /home -name "passwd"
+hostnamectl
+echo "--------Find the errors if code not found----------"
+echo "----------rewirite yaml syntax----------------------"
+echo "-----------before login enter your password---------"
+read -s password
