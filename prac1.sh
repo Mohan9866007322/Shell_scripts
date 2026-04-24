6fd752a068b0e53b6fdebf47c3a5e166d51d02ab
mohan6117vja@gmail.com <mohan6117vja@gmail.com>
2026-04-24 10:10:23 +0530

    V0.3

--- prac1.sh
+++ prac1.sh
@@ -6,3 +6,5 @@ echo "--------Find the errors if code not found----------"
 echo "----------rewirite yaml syntax----------------------"
 echo "-----------before login enter your password---------"
 read -s password
+AZcopy=KEYVALUE
+echo "-------to run the script give correct AzCopy $AZcopy pair----------------"
